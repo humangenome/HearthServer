e38db25f2f4ab1a07802023fdfa644626860d164
HumanGenome <humangenome@users.noreply.github.com>
2026-07-14 11:21:23 -0700

    Route Bellwright save requests through instance logs

--- tests/HearthServer.Tests/RconHostedServiceTests.cs
+++ tests/HearthServer.Tests/RconHostedServiceTests.cs
@@ -8,7 +8,15 @@ public sealed class RconHostedServiceTests
     [Fact]
     public void GameSaveMarkerIsScopedToTheGameplayPort()
     {
-        var path = RconHostedService.GameSaveMarkerPath(12377, @"C:\Windows");
+        var path = RconHostedService.GameSaveMarkerPath(12377, @"C:\Hearth\Logs\bw.pid", @"C:\Windows");
+
+        path.Replace('\\', '/').Should().Be("C:/Hearth/Logs/hearth_force_save_12377.marker");
+    }
+
+    [Fact]
+    public void GameSaveMarkerFallsBackToWindowsTempWithoutAPidFile()
+    {
+        var path = RconHostedService.GameSaveMarkerPath(12377, "", @"C:\Windows");
 
         path.Replace('\\', '/').Should().Be("C:/Windows/Temp/hearth_force_save_12377.marker");
     }
@@ -18,7 +26,7 @@ public sealed class RconHostedServiceTests
     [InlineData(65536)]
     public void GameSaveMarkerRejectsInvalidPorts(int port)
     {
-        var action = () => RconHostedService.GameSaveMarkerPath(port, @"C:\Windows");
+        var action = () => RconHostedService.GameSaveMarkerPath(port, @"C:\Hearth\Logs\bw.pid", @"C:\Windows");
 
         action.Should().Throw<ArgumentOutOfRangeException>();
     }
